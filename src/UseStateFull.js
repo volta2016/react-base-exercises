fc9c29628db3c1873e48f911c02ca1750cf42fea
volta2016 <giovoltadesign@gmail.com>
2021-02-11 00:43:14 -0300

    initial counter state button handleReset & initialState

--- src/UseStateFull.js
+++ src/UseStateFull.js
@@ -18,15 +18,17 @@ const ListOfClicks = ({ clicks }) => {
   return <span>{clicks.join(" - ")}</span>;
 };
 
+const INITIAL_COUNTER_STATE = {
+  left: 2,
+  right: 4,
+  //clicks: 0 sacar por [clicks, setClicks] ya tenemos el length
+  message: "message in the state"
+};
+
 const App = (props) => {
   //const [left, setLeft] = useState(0);
   //const [right, setRight] = useState(0);
-  const [counters, setCount] = useState({
-    left: 0,
-    right: 0,
-    //clicks: 0 sacar por [clicks, setClicks] ya tenemos el length
-    message: "message in the state"
-  });
+  const [counters, setCount] = useState(INITIAL_COUNTER_STATE);
 
   const [clicks, setClicks] = useState([]);
 
@@ -49,6 +51,11 @@ const App = (props) => {
     setClicks((prevClicks) => [...prevClicks, "R"]);
   };
 
+  const handleReset = () => {
+    setCount(INITIAL_COUNTER_STATE);
+    setClicks([]);
+  };
+
   return (
     <>
       <h1>Hello bootcamp class 3 </h1>
@@ -56,8 +63,13 @@ const App = (props) => {
       <button onClick={handleClickLeft}>left</button>
       <button onClick={handleClickRight}>right</button>
       {counters.right}
+      <p>
+        <button onClick={handleReset}>reset</button>
+      </p>
       <p>{counters.message}</p>
-      <p>{clicks.length}</p>
+      <p>
+        <b>Total de clicks:</b> {clicks.length}
+      </p>
       <p>
         {clicks.length === 0 ? (
           <WarningNotUsed />
